257ffbb84d268ad0d2c8fd1421aba63dd9bbfc41
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-22 19:44:26 -0300

    api available

--- src/main/java/com/devbrunorafael/github_essentials/api/controller/GithubProfileController.java
+++ src/main/java/com/devbrunorafael/github_essentials/api/controller/GithubProfileController.java
@@ -1,5 +1,21 @@
 package com.devbrunorafael.github_essentials.api.controller;
 
+import com.devbrunorafael.github_essentials.domain.model.GithubUserProfileModel;
+import com.devbrunorafael.github_essentials.domain.service.GithubUserProfileService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@AllArgsConstructor
 public class GithubProfileController {
 
+    private GithubUserProfileService githubUserProfileService;
+
+    @GetMapping("/{username}")
+    public ResponseEntity<GithubUserProfileModel> findUser(@PathVariable String username){
+         GithubUserProfileModel userProfile = this.githubUserProfileService.searchUserProfile(username);
+         return ResponseEntity.ok(userProfile);
+    }
 }
