00eec950709663793824028b3f265b4f39ca19cf
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-22 19:42:22 -0300

    model and service implemented

--- src/main/java/com/devbrunorafael/github_essentials/domain/model/GithubUserProfileModel.java
+++ src/main/java/com/devbrunorafael/github_essentials/domain/model/GithubUserProfileModel.java
@@ -1,4 +1,15 @@
 package com.devbrunorafael.github_essentials.domain.model;
 
+import lombok.Data;
+
+@Data
 public class GithubUserProfileModel {
+
+    private String login;
+    private String avatar_url;
+    private String name;
+    private String company;
+    private String blog;
+    private String location;
+
 }
--- src/main/java/com/devbrunorafael/github_essentials/domain/service/GithubUserProfileService.java
+++ src/main/java/com/devbrunorafael/github_essentials/domain/service/GithubUserProfileService.java
@@ -1,5 +1,33 @@
 package com.devbrunorafael.github_essentials.domain.service;
 
+import com.devbrunorafael.github_essentials.domain.model.GithubUserProfileModel;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+
+@Service
+@AllArgsConstructor
 public class GithubUserProfileService {
+
+    private WebClient webClient;
+
+    public GithubUserProfileModel searchUserProfile(String username){
+
+        Mono<GithubUserProfileModel> userProfileModelMono = this.webClient
+                .method(HttpMethod.GET)
+                .uri("/{username}", username)
+                .retrieve()
+                .bodyToMono(GithubUserProfileModel.class);
+
+        userProfileModelMono.subscribe(userProfileModel -> {
+            System.out.println("finished");
+        });
+
+        return userProfileModelMono.block();
+    }
 
 }
