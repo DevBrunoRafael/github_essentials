115ff21f9ee3635dd1a40550b555d84dfb378b34
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-22 19:43:29 -0300

    web client implemented

--- src/main/java/com/devbrunorafael/github_essentials/api/webclient/WebClientProject.java
+++ src/main/java/com/devbrunorafael/github_essentials/api/webclient/WebClientProject.java
@@ -1,4 +1,24 @@
 package com.devbrunorafael.github_essentials.api.webclient;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class WebClientProject {
+
+    @Value("${api.url}")
+    private String apiUrl;
+
+    @Bean
+    public WebClient webClient(WebClient.Builder webClientBuilder){
+        return webClientBuilder
+                .baseUrl(this.apiUrl)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
 }
