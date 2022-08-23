ed43bf25dfd3332f79e8a2d292ae6db354f08b35
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-22 22:13:46 -0300

    initial documentation added

--- src/main/java/com/devbrunorafael/github_essentials/GithubEssentialsApplication.java
+++ src/main/java/com/devbrunorafael/github_essentials/GithubEssentialsApplication.java
@@ -2,9 +2,6 @@ package com.devbrunorafael.github_essentials;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 
 @SpringBootApplication
 public class GithubEssentialsApplication {
--- src/main/java/com/devbrunorafael/github_essentials/api/controller/GithubProfileController.java
+++ src/main/java/com/devbrunorafael/github_essentials/api/controller/GithubProfileController.java
@@ -2,6 +2,8 @@ package com.devbrunorafael.github_essentials.api.controller;
 
 import com.devbrunorafael.github_essentials.domain.model.GithubUserProfileModel;
 import com.devbrunorafael.github_essentials.domain.service.GithubUserProfileService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
+@Api(value = "Api do github")
 public class GithubProfileController {
 
     private GithubUserProfileService githubUserProfileService;
 
     @GetMapping("/{username}")
+    @ApiOperation(value = "Retorna informações do perfil do usuário informado")
     public ResponseEntity<GithubUserProfileModel> findUserProfile(@PathVariable String username){
          GithubUserProfileModel userProfile = this.githubUserProfileService.searchUserProfile(username);
          return ResponseEntity.ok(userProfile);
